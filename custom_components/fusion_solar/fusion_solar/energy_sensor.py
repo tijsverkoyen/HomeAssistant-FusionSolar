1aed151a5d1d5338b16fb4543b27427a0bec3f41
Tijs Verkoyen <tijs@verkoyen.eu>
2024-01-26 16:42:26 +0100

    Revert "Include the check for yearEnergy and monthEnergy also. See https://github.com/tijsverkoyen/HomeAssistant-FusionSolar/issues/135"
    This reverts commit 82cb53f8e934b34855653363f71366b5aee33fba.

--- custom_components/fusion_solar/fusion_solar/energy_sensor.py
+++ custom_components/fusion_solar/fusion_solar/energy_sensor.py
@@ -5,8 +5,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, SensorStateClass
 from homeassistant.const import UnitOfEnergy
 
-from .const import ATTR_TOTAL_LIFETIME_ENERGY, ATTR_TOTAL_CURRENT_YEAR_ENERGY, ATTR_TOTAL_CURRENT_MONTH_ENERGY, \
-    ATTR_REALTIME_POWER
+from .const import ATTR_TOTAL_LIFETIME_ENERGY, ATTR_REALTIME_POWER
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -45,14 +44,10 @@ class FusionSolarEnergySensor(CoordinatorEntity, SensorEntity):
 
     @property
     def native_value(self) -> float:
-        # It seems like Huawei Fusion Solar returns some invalid data for the cumulativeEnergy, yearEnergy and
-        # monthEnergy just before midnight.
-        # So we update the value only if the system is producing power at the moment.
-        if (
-                ATTR_TOTAL_LIFETIME_ENERGY == self._attribute
-                or ATTR_TOTAL_CURRENT_YEAR_ENERGY == self._attribute
-                or ATTR_TOTAL_CURRENT_MONTH_ENERGY == self._attribute
-        ):
+        # It seems like Huawei Fusion Solar returns some invalid data for the lifetime energy just before midnight
+        # Therefore we validate if the new value is higher than the current value
+        if ATTR_TOTAL_LIFETIME_ENERGY == self._attribute:
+            # Grab the current data
             entity = self.hass.states.get(self.entity_id)
             if entity is not None:
                 try:
